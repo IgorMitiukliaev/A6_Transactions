c1bcf29b7413f2c70daa7b786c4d505f3d25c12c
wloyd <wloyd@student.21-school.ru>
2022-10-09 11:07:29 +0700

    ~

--- src/hash_table/hash_table.cc
+++ src/hash_table/hash_table.cc
@@ -1,7 +1,129 @@
 #include "hash_table.h"
 
+using s21::hash1;
+using s21::hash2;
 using s21::HashTable;
 
-auto HashTable::Get(const key_type &) -> record_type & {
-    
+int s21::HashFunctionHorner(const std::string& s, int table_size,
+                            const int key) {
+  int hash_result = 0;
+  for (int i = 0; s[i] != s.size(); ++i)
+    hash_result = (key * hash_result + s[i]) % table_size;
+  hash_result = (hash_result * 2 + 1) % table_size;
+  return hash_result;
+}
+
+int s21::hash1(const std::string& s, int table_size) {
+  return HashFunctionHorner(s, table_size, table_size - 1);
+  // ключи должны быть взаимопросты, используем числа <размер таблицы> плюс и
+  // минус один.
+};
+
+int s21::hash2(const std::string& s, int table_size) {
+  return HashFunctionHorner(s, table_size, table_size + 1);
+};
+
+template <typename T>
+HashTable<T>::HashTable() {
+  buffer_size = default_size;
+  size = 0;
+  size_all_non_nullptr = 0;
+  arr = new Node*[buffer_size];
+  for (int i = 0; i < buffer_size; ++i)
+    arr[i] = nullptr;  // заполняем nullptr - то есть если значение отсутствует,
+                       // и никто раньше по этому адресу не обращался
+}
+
+template <typename T>
+HashTable<T>::~HashTable() {
+  for (int i = 0; i < buffer_size; ++i)
+    if (arr[i]) delete arr[i];
+  delete[] arr;
+}
+
+template <typename T>
+void HashTable<T>::Resize() {
+  int past_buffer_size = buffer_size;
+  buffer_size *= 2;
+  size_all_non_nullptr = 0;
+  size = 0;
+  Node** arr2 = new Node*[buffer_size];
+  for (int i = 0; i < buffer_size; ++i) arr2[i] = nullptr;
+  std::swap(arr, arr2);
+  for (int i = 0; i < past_buffer_size; ++i) {
+    if (arr2[i] && arr2[i]->state)
+      Add(arr2[i]->value);  // добавляем элементы в новый массив
+  }
+  // удаление предыдущего массива
+  for (int i = 0; i < past_buffer_size; ++i)
+    if (arr2[i]) delete arr2[i];
+  delete[] arr2;
+}
+
+template <typename T>
+void HashTable<T>::Rehash() {
+  size_all_non_nullptr = 0;
+  size = 0;
+  Node** arr2 = new Node*[buffer_size];
+  for (int i = 0; i < buffer_size; ++i) arr2[i] = nullptr;
+  std::swap(arr, arr2);
+  for (int i = 0; i < buffer_size; ++i) {
+    if (arr2[i] && arr2[i]->state) Add(arr2[i]->value);
+  }
+  // удаление предыдущего массива
+  for (int i = 0; i < buffer_size; ++i)
+    if (arr2[i]) delete arr2[i];
+  delete[] arr2;
+}
+
+template <typename T>
+s21::key_type HashTable<T>::Find(const T& value) {
+  int h1 =
+      hash1(value, buffer_size);  // значение, отвечающее за начальную позицию
+  int h2 =
+      hash2(value, buffer_size);  // значение, ответственное за "шаг" по таблице
+  int i = 0;
+  while (arr[h1] != nullptr && i < buffer_size) {
+    if (arr[h1]->value == value && arr[h1]->state)
+      return true;  // такой элемент есть
+    h1 = (h1 + h2) % buffer_size;
+    ++i;  // если у нас i >=  buffer_size, значит мы уже обошли абсолютно все
+          // ячейки, именно для этого мы считаем i, иначе мы могли бы
+          // зациклиться.
+  }
+  return false;
+}
+
+template <typename T>
+bool HashTable<T>::Add(const T& value) {
+  if (size + 1 > int(rehash_size * buffer_size))
+    Resize();
+  else if (size_all_non_nullptr > 2 * size)
+    Rehash();  // происходит рехеш, так как слишком много deleted-элементов
+  int h1 = hash1(value, buffer_size);
+  int h2 = hash2(value, buffer_size);
+  int i = 0;
+  int first_deleted = -1;  // запоминаем первый подходящий (удаленный) элемент
+  while (arr[h1] != nullptr && i < buffer_size) {
+    if (arr[h1]->value == value && arr[h1]->state)
+      return false;  // такой элемент уже есть, а значит его нельзя вставлять
+                     // повторно
+    if (!arr[h1]->state &&
+        first_deleted == -1)  // находим место для нового элемента
+      first_deleted = h1;
+    h1 = (h1 + h2) % buffer_size;
+    ++i;
+  }
+  if (first_deleted ==
+      -1)  // если не нашлось подходящего места, создаем новый Node
+  {
+    arr[h1] = new Node(value);
+    ++size_all_non_nullptr;  // так как мы заполнили один пробел, не забываем
+                             // записать, что это место теперь занято
+  } else {
+    arr[first_deleted]->value = value;
+    arr[first_deleted]->state = true;
+  }
+  ++size;  // и в любом случае мы увеличили количество элементов
+  return true;
 }
--- src/hash_table/hash_table.h
+++ src/hash_table/hash_table.h
@@ -3,14 +3,15 @@
 #include "../core/virtual_base.h"
 
 namespace s21 {
+int HashFunctionHorner(const std::string &s, int table_size, const int key);
+int hash1(const std::string& s, int table_size);
+int hash2(const std::string& s, int table_size);
 
+template <class T>
 class HashTable : public BaseClass {
  public:
-  HashTable(const HashTable &) = default;
-  HashTable(HashTable &&) = default;
-  HashTable &operator=(const HashTable &) = default;
-  HashTable &operator=(HashTable &&) = default;
-
+  HashTable();
+  ~HashTable();
   auto Set(const record_type &) -> bool;
   auto Get(const key_type &) -> record_type &;
   auto Exist(const key_type &) -> bool;
@@ -19,11 +20,32 @@ class HashTable : public BaseClass {
   auto Keys() -> void;
   auto Rename(const key_type &, const key_type &) -> bool;
   auto TTL(const key_type &) -> int;
-  auto Find(const Person &) -> void;
+  auto Find(const T &) -> key_type;
   auto ShowAll() -> void;
   auto Upload(const std::string &) -> size_t;
   auto Export(const std::string &) -> size_t;
   auto Clear() -> void;
+  bool Add(const T &value);
+
+ private:
+  static const int default_size = 8;  // начальный размер нашей таблицы
+  constexpr static const double rehash_size =
+      0.75;  // коэффициент, при котором произойдет увеличение таблицы
+  struct Node {
+    T value;
+    bool state;  // если значение флага state = false, значит элемент массива
+                 // был удален (deleted)
+    Node(const T &value_) : value(value_), state(true) {}
+  };
+  Node **arr;  // соответственно в массиве будут хранится структуры Node*
+  int size;  // сколько элементов у нас сейчас в массиве (без учета deleted)
+  int buffer_size;  // размер самого массива, сколько памяти выделено под
+                    // хранение нашей таблицы
+  int size_all_non_nullptr;  // сколько элементов у нас сейчас в массиве (с
+                             // учетом deleted)
+
+  auto Resize() -> void;
+  auto Rehash() -> void;
 };
 
 }  // namespace s21
